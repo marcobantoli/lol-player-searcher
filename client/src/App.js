47da3c5b3657269e8ad09a7171314dff641a28f9
Marco Bantolino <marcobantoli@gmail.com>
2022-07-22 00:45:47 -0400

    Revamped state management

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,39 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import PlayerProfile from './PlayerProfile';
 import Search from './Search';
 
 function App() {
   const [player, setPlayer] = useState(null);
+  const [failedFetch, setFailedFetch] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    (async () => {
-      if (player) {
-        const response = await fetch(`/api/${player}`);
-        const data = await response.json();
-        setPlayer(data);
-      }
-    })();
-  }, [player]);
+  async function fetchData(playerName) {
+    setIsLoading(true);
+    const response = await fetch(`/api/${playerName}`);
+    const playerData = await response.json();
+    
+    setIsLoading(false);
+    if (!playerData) {
+      setFailedFetch(true);
+    } else {
+      setFailedFetch(false);
+    }
+    setPlayer(playerData);
+  }
 
   function handleSearch(e) {
-    if (e.keyCode === 13 && e.target.value !== '') {
-      setPlayer(e.target.value);
+    const playerName = e.target.value;
+    if (e.keyCode === 13 && playerName !== '') {
+      fetchData(playerName);
     }
   }
 
   return (
     <>
       <Search handleSearch={handleSearch} />
-      { player ? <PlayerProfile player={player} /> : null}
+      {player ? <PlayerProfile player={player} /> : null}
+      {failedFetch && !isLoading ? <div>Could not find the summoner</div> : null}
+      {isLoading ? <div>Loading...</div> : null}
     </>
   );
 }
