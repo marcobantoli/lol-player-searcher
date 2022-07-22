f9ac770cc43778eae6c63ec21713dac08774604d
Marco Bantolino <marcobantoli@gmail.com>
2022-07-22 13:48:52 -0400

    Implemented multi-page functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,42 @@
 import { useState } from 'react';
 import PlayerProfile from './PlayerProfile';
-import Search from './Search';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   const [player, setPlayer] = useState(null);
   const [failedFetch, setFailedFetch] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
-  async function fetchData(playerName) {
-    setIsLoading(true);
-    const response = await fetch(`/api/${playerName}`);
-    const playerData = await response.json();
-    
-    setIsLoading(false);
-    if (!playerData) {
-      setFailedFetch(true);
-    } else {
-      setFailedFetch(false);
-    }
-    setPlayer(playerData);
-  }
-
-  function handleSearch(e) {
+  function handleSearch(e, navigate) {
     const playerName = e.target.value;
+
     if (e.keyCode === 13 && playerName !== '') {
-      fetchData(playerName);
+      (async () => {
+        setIsLoading(true);
+        const response = await fetch(`/api/${playerName}`);
+        const playerData = await response.json();
+        setIsLoading(false);
+
+        if (!playerData) {
+          setFailedFetch(true);
+        } else {
+          setFailedFetch(false);
+        }
+
+        setPlayer(playerData);
+        navigate(playerName);
+      })();
     }
   }
 
   return (
-    <>
-      <Search handleSearch={handleSearch} />
-      {player ? <PlayerProfile player={player} /> : null}
-      {failedFetch && !isLoading ? <div>Could not find the summoner</div> : null}
-      {isLoading ? <div>Loading...</div> : null}
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={!isLoading ? <Home handleSearch={handleSearch} /> : <div>Loading...</div>} />
+        <Route path="/:name" element={!failedFetch ? <PlayerProfile player={player} /> : <div>Could not find the summoner</div>}/>
+      </Routes>
+    </Router>
   );
 }
 
